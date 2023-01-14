1cb54847768f0c5a5619f9df75d90c00f26d3834
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2023-01-14 14:14:04 -0500

    first commit "design square shape graphics using turtle"

---
+++ Class1/square_graphic.py
@@ -0,0 +1,39 @@
+"""
+This assigment generate square shape graphic using turtle builtin package of python.
+"""
+
+# * Import Python Package
+import turtle
+
+# * Define colour for graphics
+turtle.color("blue")
+
+# * Define size of graphics
+turtle.pensize(10)
+
+# * Move graphics to specified position
+turtle.forward(100)
+
+# * Move graphics to specified angle direction
+turtle.left(90)
+
+# * Move graphics to specified position
+turtle.forward(100)
+
+# * Move graphics to specified angle direction
+turtle.left(90)
+
+# * Move graphics to specified position
+turtle.forward(100)
+
+# * Move graphics to specified angle direction
+turtle.left(90)
+
+# * Move graphics to specified position
+turtle.forward(100)
+
+# * Move graphics to specified angle direction
+turtle.left(90)
+
+# * End the graphics
+turtle.done()
