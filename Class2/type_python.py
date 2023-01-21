b6cae6a64c62147fe2298541af17d854e9178a87
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2023-01-21 08:38:30 -0500

    assign value to varialbes and print type of it

---
+++ Class2/type_python.py
@@ -0,0 +1,14 @@
+# * Assigns values to variables and print type of the variables
+a = 23
+b = 2.5
+c = -19
+d = "ujjawal poudel"
+e = True
+f = False
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
