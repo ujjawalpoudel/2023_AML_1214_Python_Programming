c5d82c9d467e17e1dc4296c49ec48650859e2bb5
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2023-01-28 08:27:11 -0500

    dictionary class practice

---
+++ Class3/dictionary_class.py
@@ -0,0 +1,22 @@
+# * Dictionary class example
+class_dict = {"car": "Tesla", "truck": "Toyota", "bus": "Ford"}
+
+# * Print class_dict
+print(class_dict)
+
+# * Print particular value of class_dict
+print(class_dict["bus"])
+
+# * Use variable to print particular value of class_dict
+bus = class_dict["bus"]
+print(bus)
+
+# * Print all keys of class_dict
+print(class_dict.keys())
+
+# * Print all values of class_dict
+print(class_dict.values())
+
+# * Replace particular value of class_dict
+class_dict["bus"] = "Honda"
+print(class_dict)
